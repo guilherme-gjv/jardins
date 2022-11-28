d49527bc6dfa918b27f04c086a9802cb8f8592b8
guilherme-gjv <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>
2022-11-28 18:46:17 -0300

    fix: just a cool name :)

--- src/TheGarden.tsx
+++ src/TheGarden.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Plant from "./components/Plant/Plant";
 
-const Main: React.FC = () => {
+const TheGarden: React.FC = () => {
   return (
     <div className="bg-green-600 text-white text-center h-screen flex flex-col">
       <h1 className="mt-5 mb-auto text-4xl">Jardins.</h1>
@@ -14,4 +14,4 @@ const Main: React.FC = () => {
   );
 };
 
-export default Main;
+export default TheGarden;
