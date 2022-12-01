d2a62f390587c7c463ed9f8de44567d865a144f0
guilherme-gjv <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>
2022-11-30 21:52:48 -0300

    chore: mapping plants

--- src/TheGarden.tsx
+++ src/TheGarden.tsx
@@ -1,17 +1,34 @@
 import React from "react";
-import Plant from "./components/Plant/Plant";
+import Plant, { IPlantProps } from "./components/Plant/Plant";
 
 const TheGarden: React.FC = () => {
-  const date = new Date(Date.now());
-  const name = "Gui";
+  const plants: IPlantProps[] = [
+    {
+      name: "Gui",
+      date: new Date(Date.now()),
+    },
+    {
+      name: "dudo",
+      date: new Date(Date.now()),
+    },
+    {
+      name: "dudu",
+      date: new Date(Date.now()),
+    },
+    {
+      name: "jezz",
+      date: new Date(Date.now()),
+    },
+  ];
+
   return (
     <div className="bg-cyan-400 text-white text-center h-screen flex flex-col">
       <h1 className="mt-5 mb-auto text-4xl">Jardins.</h1>
       <div className="bg-cyan-300 h-[90vh] w-full pt-10 flex flex-col justify-end">
         <div className="bg-green-700 z-50 w-full h-16 px-5 pb-5 flex flex-row  items-end justify-start space-x-3">
-          <Plant name={name} date={date} />
-          <Plant name={name} date={date} />
-          <Plant name={name} date={date} />
+          {plants.map((plant) => {
+            return <Plant name={plant.name} date={plant.date} />;
+          })}
         </div>
       </div>
     </div>
