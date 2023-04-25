8332ca85737d3ac7448586ddc50230ea65430a87
guilherme-gjv <guijesus.info@gmail.com>
2023-04-25 20:03:05 -0300

    chore: improve performance with memos, states and effects

--- src/components/plant/Plant.tsx
+++ src/components/plant/Plant.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useMemo, useState } from "react";
 import Pot from "./Pot";
 import Vegetal from "./Vegetal";
 
@@ -7,17 +8,33 @@ export interface IPlantProps {
 }
 
 const Plant: React.FC<IPlantProps> = ({ name, date }) => {
+  //* constants
   const SECONDS_IN_A_DAY = 86400;
-  const numberOfVegetals: number =
-    Math.floor(
-      (new Date(Date.now()).getTime() - date.getTime()) /
-        1000 /
-        SECONDS_IN_A_DAY
-    ) + 1;
-  let vegetals = [];
-  for (let i = 0; i < numberOfVegetals; i++) {
-    vegetals.push(<Vegetal />);
-  }
+
+  //* memos
+  const numberOfVegetals: number = useMemo(() => {
+    return (
+      Math.floor(
+        (new Date(Date.now()).getTime() - date.getTime()) /
+          1000 /
+          SECONDS_IN_A_DAY
+      ) + 1
+    );
+  }, [date]);
+
+  //* states
+  const [vegetals, setVegetals] = useState<JSX.Element[]>([]);
+
+  //* effects
+  useEffect(() => {
+    const newVegetals = [];
+    for (let i = 0; i < numberOfVegetals; i++) {
+      newVegetals.push(<Vegetal />);
+    }
+    setVegetals(newVegetals);
+  }, [numberOfVegetals]);
+
+  //* render
   return (
     <div className="relative w-32">
       <div id="vegetal" className="relative flex flex-col items-center">
