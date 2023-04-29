c8a4387092920913f23ee847e885df6bbe3282f8
guilherme-gjv <guijesus.info@gmail.com>
2023-04-29 18:47:36 -0300

    chore: mockdata with miliseconds

--- src/mock/plantsMock.ts
+++ src/mock/plantsMock.ts
@@ -3,18 +3,18 @@ import { IPlantProps } from "@/components/plant/Plant";
 export const plantsMock: IPlantProps[] = [
   {
     name: "Gui",
-    date: new Date(Date.now()),
+    date: Date.now(),
   },
   {
     name: "dudo",
-    date: new Date(Date.now()),
+    date: Date.now(),
   },
   {
     name: "dudu",
-    date: new Date(Date.now()),
+    date: Date.now(),
   },
   {
     name: "jezz",
-    date: new Date(Date.now()),
+    date: Date.now(),
   },
 ];
